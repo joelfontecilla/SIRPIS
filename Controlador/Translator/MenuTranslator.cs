5a28d5e410abb2663c1330b6b2d631d9e10693bd
Javier <djavix.17@gmail.com>
2016-02-15 14:50:48 -0430

    Subiendo actualizacion del traductor de Menu

--- Controlador/Translator/MenuTranslator.cs
+++ Controlador/Translator/MenuTranslator.cs
@@ -14,16 +14,25 @@ namespace Controlador.Translator
 
         public MenuTranslator(){}
 
+        private Menu NewMenu(string ID, string NOMBRE, string NIVEL)
+        {
+            _menu = new Menu();
+            _menu.ID = ID;
+            _menu.Nombre = NOMBRE;
+            _menu.Nivel = NIVEL;
+            _menu.Activo = true;
+            return _menu;
+        }
+
         public Menu GetMenu(IDataReader param)
         {
             _menu = null;
             if (param.Read())
             {
-                _menu = new Menu();
-                _menu.ID = (!param.IsDBNull(param.GetOrdinal("ID"))) ? (param.GetString(param.GetOrdinal("ID"))) : string.Empty;
-                _menu.Nombre = (!param.IsDBNull(param.GetOrdinal("NOMBRE"))) ? (param.GetString(param.GetOrdinal("NOMBRE"))) : string.Empty;
-                _menu.Nivel = (!param.IsDBNull(param.GetOrdinal("NIVEL"))) ? (param.GetString(param.GetOrdinal("NIVEL"))) : string.Empty;
-                _menu.Activo = true;
+                string id = (!param.IsDBNull(param.GetOrdinal("ID"))) ? (param.GetString(param.GetOrdinal("ID"))) : string.Empty;
+                string nombre = (!param.IsDBNull(param.GetOrdinal("NOMBRE"))) ? (param.GetString(param.GetOrdinal("NOMBRE"))) : string.Empty;
+                string nivel = (!param.IsDBNull(param.GetOrdinal("NIVEL"))) ? (param.GetString(param.GetOrdinal("NIVEL"))) : string.Empty;
+                _menu = NewMenu(id, nombre, nivel);
             }
             return _menu;
         }
@@ -37,12 +46,10 @@ namespace Controlador.Translator
                 List<Menu> list = new List<Menu>();
                 while (param.Read())
                 {
-                    _menu = new Menu();
-                    _menu.ID = (!param.IsDBNull(param.GetOrdinal("ID"))) ? (param.GetString(param.GetOrdinal("ID"))) : string.Empty;
-                    _menu.Nombre = (!param.IsDBNull(param.GetOrdinal("NOMBRE"))) ? (param.GetString(param.GetOrdinal("NOMBRE"))) : string.Empty;
-                    _menu.Nivel = (!param.IsDBNull(param.GetOrdinal("NIVEL"))) ? (param.GetString(param.GetOrdinal("NIVEL"))) : string.Empty;
-                    _menu.Activo = true;
-                    list.Add(_menu);
+                    string id = (!param.IsDBNull(param.GetOrdinal("ID"))) ? (param.GetString(param.GetOrdinal("ID"))) : string.Empty;
+                    string nombre = (!param.IsDBNull(param.GetOrdinal("NOMBRE"))) ? (param.GetString(param.GetOrdinal("NOMBRE"))) : string.Empty;
+                    string nivel = (!param.IsDBNull(param.GetOrdinal("NIVEL"))) ? (param.GetString(param.GetOrdinal("NIVEL"))) : string.Empty;
+                    list.Add(NewMenu(id, nombre, nivel));
                 }
                 _menus = list.ToArray();
             }
